2c0eb1846eef3115b21ea10ad0c25cd910ef6cc6
xiaomu <zxy1985938323@163.com>
2024-04-15 17:11:10 +0800

    I hate 毕设  very very much

--- Predict/urls.py
+++ Predict/urls.py
@@ -5,4 +5,5 @@ app_name='Predict'
 
 urlpatterns = [
     path('',views.grades_list,name='grades_list'),
+    path('g_add/',views.add_grades,name='add_grades'),
 ]
--- Predict/views.py
+++ Predict/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Prediction
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def grades_list(request):
     grades=Prediction.objects.order_by('test_time')
-    return render(request,'adding.html',{'grades':grades})
+    return render(request,'Pre/adding.html',{'grades':grades,'section':'grades_list'})
+
+@login_required
+def add_grades(request):
+    if request.method == 'POST':
+        # 如果表单被提交，处理表单数据
+        test_time = request.POST.get('test_time')
+        avg_grade = request.POST.get('avg_grade')
+        
+        # 创建grade对象并保存到数据库
+        Prediction.objects.create(
+            test_time=test_time,
+            avg_grade=avg_grade,
+        )
+        # 重定向到grades列表页面
+        return redirect('Predict:grades_list')
+    else:
+        # 如果是GET请求，返回空的表单
+        return render(request, 'Predict/adding.html')
